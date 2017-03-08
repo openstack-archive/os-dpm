a6df6b88d864f3e8a663390bd315abbfd6ed0285
Andreas Scheuring <andreas.scheuring@de.ibm.com>
2017-03-08 09:35:00 +0100

    Add DPMObjectId configuration object
    This patch adds a new DPMObjectId configuraiton object and
    corresponding type to os-dpm. This new configuration object
    represents a DPM object id. It inlcudes validation if the
    given config value has the correct object-id format.
    
    It allows upper and lower case values to be defined, however
    internally it converts everything to lower case (which is required by
    the hmc webservices).
    
    Example for a valid object-id:
      fa1f2466-12df-311a-804c-4ed2cc1d656b
    
    Change-Id: I6b446f9634a018e005e112cf1e3f9cc7f6fb4a1b

--- os_dpm/config/types.py
+++ os_dpm/config/types.py
@@ -15,6 +15,26 @@
 
 from oslo_config import cfg
 
+from os_dpm import constants as const
+
+
+class DPMObjectIdType(cfg.types.String):
+    def __init__(self):
+        """DPM Object-Id Type.
+
+        DPM Object-Id values are returned as lower case str objects.
+        If the configuration value is provided as upper case, this type
+        converts it to lower case.
+        """
+
+        super(DPMObjectIdType, self).__init__(
+            type_name='DPM Object-Id value', ignore_case=True, max_length=36,
+            regex="^" + const.OBJECT_ID_REGEX + "$")
+
+    def __call__(self, value):
+        val = super(DPMObjectIdType, self).__call__(value)
+        return val.lower()
+
 
 class MultiStringWithKwargsType(cfg.types.String):
     """Multi String with kwargs type
